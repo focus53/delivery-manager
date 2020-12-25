84884fed6d49ba6ebbfebeea41d1f2d42542cc96
unknown <ASUS@DESKTOP-BH1VUVV.(none)>
2020-12-25 11:33:26 +0100

    fix: Calendar to func comp

--- client/src/components/Calendar/Calendar.jsx
+++ client/src/components/Calendar/Calendar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactCalendar from 'react-calendar';
 import { connect } from 'react-redux';
 
@@ -6,46 +6,37 @@ import 'react-calendar/dist/Calendar.css';
 import './OwnCalendar.css';
 import { selectDateTC, setDateTC } from '../Redux/address-reducer';
 
-class CalendarClass extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { selectedDate: new Date() };
-  }
-  componentDidMount() {
-    this.props.setDateTC(this.state.selectedDate.toDateString(), this.props.token);
-    this.props.selectDateTC(this.state.selectedDate.toDateString());
-  }
+const Calendar = (props) => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
-  handleChange(val) {
-    this.setState({ selectedDate: val });
-    this.props.selectDateTC(val.toDateString());
-  }
+  useEffect(() => {
+    props.setDateTC(selectedDate.toDateString(), props.token);
+  }, []);
+  useEffect(() => {
+    props.selectDateTC(selectedDate.toDateString());
+  }, [selectedDate]);
 
-  handleClick(value, event) {
-    //props.haveAddressTC(value.toDateString());
-  }
-  render() {
-    return (
-      <div>
-        <div className="row">
-          <div className="col s12 m6">
-            <ReactCalendar
-              onChange={(val) => {
-                this.handleChange(val);
-              }}
-              value={this.selectedDate}
-              className="card blue-grey darken-1 ownStyleCalendar"
-              onClickDay={this.handleClick}
-              tileClassName={(obj) => {
-                return this.props.haveAddress.includes(obj.date.toDateString()) && 'haveAddress';
-              }}
-            />
-          </div>
-        </div>
+  const handleChange = (val) => {
+    setSelectedDate(val);
+  };
+
+  return (
+    <div className="row">
+      <div className="col s12 m6">
+        <ReactCalendar
+          onChange={(val) => {
+            handleChange(val);
+          }}
+          value={selectedDate}
+          className="card blue-grey darken-1 ownStyleCalendar"
+          tileClassName={(obj) => {
+            return props.haveAddress.includes(obj.date.toDateString()) && 'haveAddress';
+          }}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
@@ -55,4 +46,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, { selectDateTC, setDateTC })(CalendarClass);
+export default connect(mapStateToProps, { selectDateTC, setDateTC })(Calendar);
